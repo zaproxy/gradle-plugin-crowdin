d7a65ec2aa279d481ea7164ae0703c4bd8b9a6b2
thc202 <thc202@gmail.com>
2021-07-30 13:31:23 +0100

    Release version 0.2.0
    Update CHANGELOG and build.gradle.kts.
    
    Signed-off-by: thc202 <thc202@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ repositories {
 }
 
 group = "org.zaproxy.gradle"
-version = "0.2.0-SNAPSHOT"
+version = "0.2.0"
 
 tasks.withType<PluginUnderTestMetadata>().configureEach {
     pluginClasspath.from(configurations.compileOnly)
